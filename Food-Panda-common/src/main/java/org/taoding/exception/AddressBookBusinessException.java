c8a392e3e73474f7b924c29f0b61d059ad964dd9
“DtNewbee0619” <9318264@gmail.com>
2024-06-22 18:39:25 +0800

    first commit

---
+++ Food-Panda-common/src/main/java/org/taoding/exception/AddressBookBusinessException.java
@@ -0,0 +1,9 @@
+package org.taoding.exception;
+
+public class AddressBookBusinessException extends BaseException {
+
+    public AddressBookBusinessException(String msg) {
+        super(msg);
+    }
+
+}
